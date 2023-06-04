b31f8d0b9f66bd971cf77f833b359aaff2a37fc0
Mulosbron <91866065+mulosbron@users.noreply.github.com>
2023-06-04 16:17:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from setuptools import setup
 setup(
 	name='balikAvla',
 	version='1.0',
-	url='https://github.com/mulosbron/balikAvla',
+	url='https://github.com/mulosbron/BalikAvla',
 	author='mulosbron',
 	author_email='mulosbron@gmail.com',
 	description='balık yakalama oyunu',
 	install_requires=[
 		'pygame',
 		'random',
-		'mysql.connector',
 		'datetime',
 	],
-	py_modules=['balikAvlamaOyunu', 'dbSkorlar'],
+	py_modules=['balikAvlamaOyunu', 'skorTablosu'],
 )
